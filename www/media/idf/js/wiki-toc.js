29d966cdb3178963702a87919ad3e4ae496e510c
Thomas Keller <me@thomaskeller.biz>
2011-03-01 00:21:05 +0100

    Reformat slightly; hide the table of contents if there are less than two jump marks.

--- www/media/idf/js/wiki-toc.js
+++ www/media/idf/js/wiki-toc.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
-    $(":header", "#wiki-content").map(function (index) {
+    $(":header", "#wiki-content").map(function(index) {
         this.id = "wikititle_" + index;
-        $("<a href='#" + this.id + "'>" + jQuery.fn.text([this]) + "</a>").addClass("wiki-" + this.tagName.toLowerCase()).appendTo('#wiki-toc-content');
-    });    
+        $("<a href='#" + this.id + "'>" + jQuery.fn.text([this]) + "</a>")
+            .addClass("wiki-" + this.tagName.toLowerCase())
+            .appendTo('#wiki-toc-content');
+    });
+    if ($('#wiki-toc-content').size() < 2)
+        $('#wiki-toc').hide();
 });
 
