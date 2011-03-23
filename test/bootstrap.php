53ab5b6affa9557eef5b6921611cdf0e5df95ac1
Thomas Keller <me@thomaskeller.biz>
2011-03-24 00:41:22 +0100

    Expand the bootstrap script to setup an empty IDF environment.

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,7 +1,28 @@
 <?php
 
+$config_file =  dirname(__FILE__) . '/config.php';
+if (!file_exists($config_file)) {
+    die("'test/config.php' does not exist\n");
+}
+
+echo ">>> setting paths...\n";
 define('SRCDIR', realpath(dirname(__FILE__) . '/../src'));
 define('TESTDIR', dirname(__FILE__));
 define('DATADIR', TESTDIR . '/data');
-
 set_include_path(get_include_path() . PATH_SEPARATOR . SRCDIR);
+
+$testconfig = require_once $config_file;
+if (file_exists($testconfig['db_database'])) {
+    echo ">>> removing any existing database\n";
+    unlink($testconfig['db_database']);
+}
+
+echo ">>> creating empty test database...\n";
+passthru('php ' . PLUF_PATH . '/migrate.php --conf=' . TESTDIR . '/config.php -a -i');
+
+echo ">>> setting up web application...\n";
+require 'Pluf.php';
+// for some reason this is nowhere done in Pluf.php
+spl_autoload_register('__autoload');
+Pluf::start(TESTDIR . '/config.php');
+
