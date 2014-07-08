f11ebbcb740a6e817f48d0b4cd5e20ee7162f3f8
yhfudev <yhfudev@gmail.com>
2014-07-08 11:38:41 -0400

    srchub: Fixing APC with latest version of PHP

--- src/IDF/Views/Issue.php
+++ src/IDF/Views/Issue.php
@@ -62,10 +62,13 @@ class IDF_Views_Issue
              'id' => __('Id'),
              array('summary', 'IDF_Views_Issue_SummaryAndLabels', __('Summary')),
              array('status', 'IDF_Views_Issue_ShowStatus', __('Status')),
+             array('submitter', 'Pluf_Paginator_FkToString', __('submitter')),
+             array('owner', 'Pluf_Paginator_FkToString', __('owner')),
              array('due_dtime', 'IDF_Views_Issue_DueDate', __('Due Date')),
              array('modif_dtime', 'Pluf_Paginator_DateAgo', __('Last Updated')),
                               );
         $pag->configure($list_display, array(), array('id', 'status', 'due_dtime', 'modif_dtime'));
+        $pag->configure($list_display, array(), array('id', 'status', 'owner', 'submitter', 'due_dtime', 'modif_dtime'));
         $pag->items_per_page = 10;
         $pag->no_results_text = __('No issues were found.');
         $pag->setFromRequest($request);
